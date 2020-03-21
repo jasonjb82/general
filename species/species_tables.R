b8aefea75884e7a71be4c1c160fd6df0f5f131f4
Jason Benedict <jasonjb82@gmail.com>
2020-03-20 17:24:45 -0700

    Update species_tables.R
    cleaning up code

--- species/species_tables.R
+++ species/species_tables.R
@@ -18,9 +18,6 @@ df <- pdf_text(pdf_file) %>%
     as_tibble(.name_repair = make.names) %>%
     slice(16:100) %>%
     mutate(
-    #CDI = str_sub(X,0,2), 
-    #Code = str_sub(X,3,9),
-    #Text = str_sub(X,10,100)) %>%
       CDI = str_sub(X,0,9), 
       code = str_sub(X,10,25),
       text = str_sub(X,26,100)) %>%
@@ -34,6 +31,7 @@ df <- pdf_text(pdf_file) %>%
     mutate_if(is.character, list(~na_if(.,""))) %>%
     fill(CDI,code) %>%
     mutate(page = pdf_file) %>%
+    replace(is.na(.), "")
     mutate(CDI = ifelse(is.na(CDI),0,CDI))
 
 # rest of the pages
@@ -60,9 +58,6 @@ data <- pdf_text(pdf_file) %>%
     page = pdf_file,
     text = str_replace(text, "I ",""),
     code = str_replace(code," ","")) %>%
-    #CDI = str_sub(X,0,8), 
-    #Code = str_sub(X,13,20),
-    #Text = str_sub(X,21,100)) %>%
   # remove original string
   select(-X) %>%
   slice(2:100) %>%
@@ -71,6 +66,7 @@ data <- pdf_text(pdf_file) %>%
   #filter(!(CDI=="CD")) %>%
   as.data.frame() %>%
   mutate_if(is.character, list(~na_if(.,""))) %>%
+  mutate(CDI = ifelse(!is.na(code) & is.na(CDI),"",CDI)) %>%
   mutate(code = str_pad(code,4, side = 'right', pad = 'I')) %>%
   fill(CDI,code) 
 
@@ -79,10 +75,12 @@ data <- pdf_text(pdf_file) %>%
 
 merge_df <- rbind(combined_df,df)
 
-# Clean up table
+# clean up table ----------------------------------------------------------
 comb_df <- merge_df %>%
   group_by(CDI,code) %>% 
   select(-page) %>%
+  mutate(text = str_replace(text,'[*]','')) %>%
+  mutate(text = str_replace(text,'[.]','')) %>%
   summarise_all(funs(paste(., collapse = " ; "))) %>%
   separate(text, c("name", "common_name","common_name1","common_name2","common_name3"), " ; ") %>%
   mutate(common_name = ifelse(!is.na(common_name1),paste0(common_name," ",common_name1),common_name)) %>%
@@ -94,8 +92,9 @@ comb_df <- merge_df %>%
   mutate(genus = ifelse(grepl('SP$',code),paste0(genus," ",species),genus),
          species = ifelse(grepl('SP$',code),NA,species)) %>%
   mutate(species = ifelse(grepl('^L.',authority),paste0(species," ",authority),species),
-         authority = ifelse(grepl('^L.',authority),NA,authority))
-  #separate(common_name,into = c("authority2","common_name_add"), sep = "(?<=\\))",convert = TRUE, remove=FALSE,fill="right") %>%
+         authority = ifelse(grepl('^L.',authority),NA,authority)) %>%
+  mutate(authority = str_replace(authority,"[']",''))
+#separate(common_name,into = c("authority2","common_name_add"), sep = "(?<=\\))",convert = TRUE, remove=FALSE,fill="right") %>%
   #mutate(authority = ifelse(!is.na(common_name_add),paste0(authority," ",authority2),authority))
 
          
