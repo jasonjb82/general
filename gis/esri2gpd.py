2ab4c998efe28dfb4301f05f152d88f279821774
Jason Benedict <jasonjb82@gmail.com>
2020-04-06 08:44:31 -0700

    Update esri2gpd.py
    update

--- gis/esri2gpd.py
+++ gis/esri2gpd.py
@@ -10,8 +10,8 @@ import esri2gpd
 fields= ['kode_prov,fungsi,fungsi_kws,sk_kawasan,lindung']
 
 url = "https://gis.wwf.id/server/rest/services/National/Kawasan_Hutan_Indonesia_KLHK_2014/FeatureServer/0"
-gdf = esri2gpd.get(url, fields=fields, where="kode_prov='92'")
+gdf = esri2gpd.get(url, fields=fields, where="kode_prov='18'")
 
 gdf.head()
 
-gdf.to_file("D:/z_Temp/klhk/kh_2014/kh_prov15.shp")
+gdf.to_file("D:/z_Temp/klhk/kh_2014/kh_prov13.shp")
